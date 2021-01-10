a1c14c798b26eda0b76048ad14971b6b21528992
imithu <imithu@imithu.com>
2021-01-10 07:44:07 +0600

    changed -  all, country_name, dialing_code

--- UM/Database/General_country.php
+++ UM/Database/General_country.php
@@ -15,62 +15,61 @@ class General_country
      * 
      * @return array all data of all countries
      * 
-     * @version 0.0.0
+     * @version 0.1.0
      * @since 0.0.0
      * @author Mahmudul Hasan Mithu
      */
     public static function all()
     {
-        return DB::select('select * from UM_general_country;');
+        return DB::select('select * from UM_general_country ORDER BY country_name ASC;');
     }
 
 
     /**
      * 
-     * get country_name by country id
+     * get country_name by letters_2
      * 
-     * @param int $country_id
+     * @param string $letters_2
      * 
      * @return string $country_name
      * 
-     * @version 0.0.0
+     * @version 0.1.0
      * @since 0.0.0
      * @author Mahmudul Hasan Mithu
      */
-    public static function country_name( int $country_id ): string
+    public static function country_name( string $letters_2 ): string
     {
         $countries = self::all();
 
         foreach( $countries as $country ){
-            if( $country->id==$country_id ){
+            if( $country->letters_2==strtoupper($letters_2) ){
                 $country_name= $country->country_name;
                 break;
             }
         }
-
         return $country_name;
     }
 
 
     /**
      * 
-     * get dialing_code of a country by country id
+     * get dialing_code by letters_2
      * 
-     * @param int $country_id
+     * @param string $letters_2
      * 
      * @return string $dialing_code
      * 
-     * @version 0.0.0
+     * @version 0.1.0
      * @since 0.0.0
      * @author Mahmudul Hasan Mithu
      */
-    public static function dialing_code( int $country_id ): string
+    public static function dialing_code( string $letters_2 ): string
     {
         $countries = self::all();
 
         $dialing_code='+000';
         foreach( $countries as $country ){
-            if( $country->id==$country_id ){
+            if( $country->letters_2==strtoupper($letters_2) ){
                 $dialing_code= json_decode($country->dialing_code)[0];
                 break;
             }
@@ -78,4 +77,7 @@ class General_country
 
         return $dialing_code;
     }
+
+
+
 }
