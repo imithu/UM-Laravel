1536b560755d8a814e20b1de3169b454b8d498c6
imithu <imithu@imithu.com>
2021-01-17 14:46:35 +0600

    get() has renamed to select()

--- UM/Database/Options.php
+++ UM/Database/Options.php
@@ -17,16 +17,17 @@ class Options
     
 
     /**
-     * get array value from options table
+     * select array value from options table
      * 
      * @param string $meta_key
+     * 
      * @return array $meta_value
      * 
-     * @version 0.0.0
+     * @version 1.0.0
      * @since 0.0.0
      * @author Mahmudul Hasan Mithu
      */
-    public static function get( $meta_key )
+    public static function select( $meta_key )
     {
         $meta_value = DB::select('SELECT meta_value FROM UM_options WHERE `meta_key`=?', [$meta_key])[0]->meta_value;
 
