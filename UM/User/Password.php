df3e892abfce951b290f80ec22d39529f045d449
imithu <imithu@imithu.com>
2021-01-18 13:30:35 +0600

    password_reset_request() and password_update() have added

--- UM/User/Password.php
+++ UM/User/Password.php
@@ -2,10 +2,11 @@
 
 namespace UM\User;
 
-use UM\Misc\Dynamic_Data;
-use UM\User\Common;
+
+use UM\Verify\User;
+use UM\Verify\Syntax;
+use UM\Generate\Unknown_Data;
 use UM\Database\Users;
-use UM\Database\Usermeta;
 
 
 class Password
@@ -13,8 +14,7 @@ class Password
 
     /**
      * 
-     * password reset step 1
-     * in step 1,
+     * password reset request
      * 
      * 
      * @param string username_or_email
@@ -23,76 +23,64 @@ class Password
      *                 else []
      * 
      * 
-     * @version 0.0.0
-     * @since 0.0.0
-     * @author Mahmudul Hasan Mithu
+     * @since   0.0.0
+     * @version 1.0.0
+     * @author  Mahmudul Hasan Mithu
      */
-    public static function password_reset_step_1( $username_or_email )
+    public static function password_reset_request( string $username_or_email )
     {
+        $SR = [];
         $user_id = Users::id_username_or_email( $username_or_email );
 
-        if( $user_id==false ){
-            return [];
-        }elseif( $user_id>0 ){
-            $user_is_verified = Common::user_is_verified( $user_id );
-            if( $user_is_verified==false ){
-                return [];
-            }elseif( $user_is_verified==true ){
-                $temp_otp = Dynamic_Data::temp_otp(6);
-                $email = Users::email_id( $user_id );
+        if( $user_id>0 && User::user_is_verified( $user_id ) ){
+            $temp_otp = Unknown_Data::temp_otp(6);
+            $email = Users::select( $user_id, 'email' );
+            Users::update( $user_id, 'temp_otp', $temp_otp );
 
-                Usermeta::set( $user_id, 'temp_otp', $temp_otp );
-
-                return [ 'user_id'=>$user_id, 'email'=>$email, 'temp_otp'=>$temp_otp];
-            }
+            $SR = 
+            [
+                'user_id'=>$user_id,
+                'email'=>$email,
+                'temp_otp'=>$temp_otp
+            ];
         }
 
-        return [];
+        return $SR;
     }
 
 
+
+
     /**
-     * password reset step 2
-     * in step 2,
-     * match the temp_otp and do many things 
-     * then change the password
+     * update password of an user
      * 
-     * @param int $user_id
+     * @param int    $user_id
      * @param string $temp_otp
      * @param string $password
      * 
-     * @return bool - true  - password change is successful
-     *                false - password change is failed
+     * @return bool - true  - password update successful
+     *                false - password update failed
      * 
-     * @version 0.0.0
-     * @since 0.0.0
-     * @author Mahmudul Hasan Mithu
+     * @since   0.0.0
+     * @version 1.0.0
+     * @author  Mahmudul Hasan Mithu
      */
-    public static function password_reset_step_2( int $user_id, string $temp_otp, string $password )
+    public static function password_update( int $user_id, string $temp_otp, string $password )
     {
-        if( strlen($password)<8 ){
-            return false;
-        }elseif( strlen($password)>7 ){
-            $user_is_verified = Common::user_is_verified( $user_id );
-            if( $user_is_verified==false ){
-                return false;
-            }elseif( $user_is_verified==true ){
-                $db_temp_otp = Usermeta::get( $user_id, 'temp_otp' );
-                if( $db_temp_otp=='no_otp' ){
-                    return false;
-                }elseif( $db_temp_otp!='no_otp' ){
-                    if( $temp_otp!=$db_temp_otp ){
-                        return false;
-                    }elseif( $temp_otp==$db_temp_otp ){
-                        Users::password_hashed_set( $user_id, $password );
-                        Usermeta::set( $user_id, 'temp_otp', 'no_otp' );
-                        return true;
-                    }
-                }
-            }
+        $SR = false;
+
+        if( 
+               Syntax::password($password) 
+            && $user_id>0 
+            && User::user_is_verified( $user_id ) 
+            && User::temp_otp($user_id, $temp_otp) 
+        )
+        {
+            Users::update( $user_id, 'password', password_hash($password, PASSWORD_DEFAULT) );
+            $SR = true;
         }
 
-        return false;
+        return $SR;
     }
 
 }
