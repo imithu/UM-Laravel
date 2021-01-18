830a8645efcd426eabc023f191e1f92a67ca624c
imithu <imithu@imithu.com>
2021-01-18 08:05:29 +0600

    temp_otp() has added

--- UM/Verify/User.php
+++ UM/Verify/User.php
@@ -9,6 +9,37 @@ use Illuminate\Support\Facades\DB;
 
 class User{
     /**
+     * verify a user by temp_otp
+     * 
+     * @param int    $user_id
+     * @param string $temp_otp
+     * 
+     * @return boolean - true  if temp_otp verification is successful
+     *                 - false if temp_otp verification is failed
+     *  
+     * 
+     * 
+     * 
+     * @since   1.0.0
+     * @version 1.0.0
+     * @author  Mahmudul Hasan Mithu
+     */
+    public static function temp_otp( int $user_id, string $temp_otp)
+    {
+        $db_temp_otp = Users::select( $user_id, 'temp_otp' );
+        if( $db_temp_otp!='no_otp' && $db_temp_otp!='' && $db_temp_otp!=NULL && $db_temp_otp!=false ){
+            if( $temp_otp==$db_temp_otp ){
+                Users::update( $user_id, 'temp_otp', 'no_otp' );
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    /**
      * check a user is verified or not
      * 
      * @param int $user_id
