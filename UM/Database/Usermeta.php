b239c040f0955dff6e8ea046c2eac39ce9d8e4e9
imithu <imithu@imithu.com>
2021-02-10 21:40:24 +0600

    unique parameter has been added in insert method

--- UM/Database/Usermeta.php
+++ UM/Database/Usermeta.php
@@ -15,16 +15,28 @@ class Usermeta
      * @param int    $user_id
      * @param string $meta_key
      * @param string $meta_value
+     * @param bool   $unique (optional)
      * 
      * @since   0.0.0
-     * @version 1.0.0
+     * @version 1.6.0
      * @author  Mahmudul Hasan Mithu
      */
-    public static function insert( int $user_id, string $meta_key, string $meta_value )
+    public static function insert( int $user_id, string $meta_key, string $meta_value, bool $unique=true )
     {
         date_default_timezone_set('UTC');
         $datetime = date('Y-m-d H:i:s');
-        DB::insert('INSERT INTO UM_usermeta  VALUES( NULL, ?, ?, ?, ?)', [$user_id, htmlspecialchars(trim($meta_key)), htmlspecialchars(trim($meta_value)), $datetime]);
+
+        if( $unique ){
+            if(count(DB::select('SELECT * FROM UM_usermeta WHERE (user_id=? AND meta_key=?) ORDER BY id DESC', [$user_id, htmlspecialchars(trim($meta_key))]))==0){
+                DB::insert('INSERT INTO UM_usermeta  VALUES( NULL, ?, ?, ?, ?)', [$user_id, htmlspecialchars(trim($meta_key)), htmlspecialchars(trim($meta_value)), $datetime]);
+            }else{
+                if(self::select($user_id, $meta_key)!=htmlspecialchars(trim($meta_value))){
+                    DB::update('UPDATE UM_usermeta SET meta_value=?, `datetime`=? WHERE (user_id=? AND meta_key=?)', [ htmlspecialchars(trim($meta_value)), $datetime, $user_id, htmlspecialchars(trim($meta_key)) ]);
+                }
+            }
+        }elseif( $unique==false ){
+            DB::insert('INSERT INTO UM_usermeta  VALUES( NULL, ?, ?, ?, ?)', [$user_id, htmlspecialchars(trim($meta_key)), htmlspecialchars(trim($meta_value)), $datetime]);
+        }
     }
 
 
